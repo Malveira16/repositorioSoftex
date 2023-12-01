75fd0221564de30b3e3e9e9a84c72cd5ee46c63b
João Vitor <joa.malveira@gmail.com>
2023-11-30 21:41:08 -0300

    Padrão Singleton

---
+++ Documents/estudos/exercícios softex/disciplina 3/BaseSecreta(Singleton)/app.ts
@@ -0,0 +1,6 @@
+const sistemaSeguranca = SistemaSeguranca.getInstance();
+
+
+sistemaSeguranca.acessarBaseSecreta("SenhaIncorreta"); 
+
+sistemaSeguranca.acessarBaseSecreta("SenhaSuperSecreta123"); 
